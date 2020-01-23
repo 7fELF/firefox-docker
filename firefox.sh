1b7a694d8dc72f0ca24b00ff3709486e2bde6a91
Antoine Baudrand <antoine.baudrand@epitech.eu>
2020-01-23 09:42:25 +0100

    Use docker's shm-size flag && remove old flags

--- firefox.sh
+++ firefox.sh
@@ -6,16 +6,14 @@ docker run --rm \
     --env=USER_UID="$(id -u)" \
     --env=USER_GID="$(id -g)" \
     --env=DISPLAY="unix$DISPLAY" \
-    --env=GTK_THEME=Flatabulous \
     --device /dev/dri \
     -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
     -v "/run/user/$(id -u)/pulse:/run/pulse:ro" \
     -v "$HOME/.themes:/usr/share/themes:ro" \
     -v /usr/share/icons/:/usr/share/icons/:ro \
     -v /etc/localtime:/etc/localtime:ro \
-    -v /dev/shm:/dev/shm \
+	--shm-size 2G \
     --name firefox$RANDOM \
     firefox "$@"
 
 # -v "$HOME/mozilla:/home/firefox/.mozilla" \
-# -v $HOME/.Xauthority:/home/firefox/.Xauthority \
